7af3a5eab9a0c466ac83ecde726616d1542b4e6c
Inma_Burlo <inmaburlo@gmail.com>
2020-02-02 14:58:48 +0100

    15) Version comentada

--- geometria/src/Rectangulo_IBR.java
+++ geometria/src/Rectangulo_IBR.java
@@ -1,21 +1,43 @@
+/**
+ * 
+ * @author Inma Burlo De Rojas
+ * V.1.2
+ *
+ */
 
 public class Rectangulo_IBR extends FiguraGeometrica_IBR {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo_IBR(String tipoFigura, double lG, double lP) {
+	/**
+	 * Contructor parametrizado de la clase, introduciendo tipo de
+	 * figura, valor del lado grande y valor del lado pequeño. 
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
+	public Rectangulo_IBR(String tipoFigura, double lG, double lP) 
+	{
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
+	/**
+	 * Calcula área con el valor de los lados
+	 */
 	@Override
-	public double area() {
+	public double area() 
+	{
 		return l1 * l2;
 	}
 	
+	/**
+	 * Calcula perímetro con el valor de los lados.
+	 */
 	@Override
-	public double perimetro() {
+	public double perimetro() 
+	{
 		return (2 * l1) + (2 * l2);
 	}
 
