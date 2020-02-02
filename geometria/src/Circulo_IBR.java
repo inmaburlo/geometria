10bc598ab43ba41848e236d865fdb83ee44499e9
Inma_Burlo <inmaburlo@gmail.com>
2020-02-02 12:09:59 +0100

    6) Version refactorizada

--- geometria/src/Circulo_IBR.java
+++ geometria/src/Circulo_IBR.java
@@ -1,10 +1,10 @@
 //Version inicial
-public class Circulo extends FiguraGeometrica {
+public class Circulo_IBR extends FiguraGeometrica_IBR {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_IBR(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_IBR.java
+++ geometria/src/FiguraGeometrica_IBR.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_IBR {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_IBR(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
